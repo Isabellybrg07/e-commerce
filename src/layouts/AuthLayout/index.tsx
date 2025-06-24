bb4aed6f675c29a6f614c99e0b5cf37a9b7c9c61
Larissa <larissamendesr@gmail.com>
2025-06-24 20:41:10 -0300

    novas pags de login

--- src/layouts/AuthLayout/index.tsx
+++ src/layouts/AuthLayout/index.tsx
@@ -4,9 +4,10 @@ import "./index.css";
 
 const AuthLayout = () => {
   return (
-    <div className="auth-layout">
-      <div>
+        <div className="auth-layout">
+    <div className="header">
       <img src={"/public/images/logo.png"} alt="Logo Isabelly-Kids" />
+       <h1>Isabelly Kids</h1>
       </div>
       <Outlet />
     </div>
